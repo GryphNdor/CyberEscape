8ced26b31fa2cf239e7a206e35542ca93685afe2
Branden Yi <js77206@gmail.com>
2024-11-10 18:05:24 -0500

    Adding level select screen

--- scenes/screens/main_menu.gd
+++ scenes/screens/main_menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 @onready var start_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Start_Button as Button
 @onready var exit_button: Button = $MarginContainer/HBoxContainer/VBoxContainer/Exit_Button as Button
-@onready var start_level = preload("res://scenes/world.tscn") as PackedScene
+@onready var level_select_screen = preload("res://scenes/screens/level_select_screen.tscn") as PackedScene
 
 func _ready():
 	start_button.button_down.connect(on_start_pressed)
@@ -11,7 +11,7 @@ func _ready():
 
 
 func on_start_pressed() -> void:
-	get_tree().change_scene_to_packed(start_level)
+	get_tree().change_scene_to_packed(level_select_screen)
 	
 
 func on_exit_pressed() -> void:
